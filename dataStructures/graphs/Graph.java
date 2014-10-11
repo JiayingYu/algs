454dc6663df387c8dd65b6b0fc944fd375c7c870
Jiaying Yu <jiaying.yu@nyu.edu>
2014-10-10 22:29:48 -0400

    dfs

--- dataStructures/graphs/Graph.java
+++ dataStructures/graphs/Graph.java
@@ -1,10 +1,13 @@
 package graphs;
 
+import java.util.Stack;
+
 public class Graph {
 	private final int MAX_VERTS = 20;
 	private Vertex[] vertexList;
 	private int[][] adjMat;
 	private int nVerts; //current number of vertices
+	private Stack<Integer> stack; //for depth first search
 	
 	public Graph() {
 		vertexList = new Vertex[MAX_VERTS];
@@ -28,5 +31,35 @@ public class Graph {
 	
 	public void displayVertex(int v) {
 		System.out.println(vertexList[v].label);
+	}
+	
+	public void dfs() {
+		vertexList[0].wasVisited = true;
+		displayVertex(0);
+		stack.push(0);
+		
+		while(!stack.isEmpty()) {
+			int v = getAdjUnvisitedVertex(stack.peek());
+			if (v == -1) { // if not exists a adjacent vertex, pop the current vertex
+				stack.pop();
+			} else {
+				vertexList[v].wasVisited = true;
+				stack.push(v);
+				displayVertex(v);
+			}
+		}
+		
+		for (int i = 0; i < nVerts; i++) {
+			vertexList[i].wasVisited = false;
+		}
+	}
+	
+	private int getAdjUnvisitedVertex(int v) {
+		for (int j = 0; j < nVerts; j++) {
+			if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
+				return j; //if found, return the index of the adjacent vertex
+			}
+		}
+		return -1;
 	}
 }
